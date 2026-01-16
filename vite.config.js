022e1b355224d4170fcc0f8cb2d7f65dad6f03c9
Jose Alicio Dias dos Santos Junior <josealicioJ@seed.se.gov>
2026-01-16 16:32:30 -0300

    Migração para Supabase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. Aumenta o limite do aviso para 1000kb (1mb) para não poluir o terminal
+    chunkSizeWarningLimit: 1000, 
+    
+    // 2. Configuração Profissional: Divide as bibliotecas em arquivos separados
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Se o arquivo vier da pasta node_modules (bibliotecas), cria um arquivo separado
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
